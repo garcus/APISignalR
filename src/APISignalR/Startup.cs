e848ed3a3e6319c04af1c9535e4ab31a88853c28
marcus <marcus@DESKTOP-GR427B9>
2017-02-05 21:27:41 +0100

    Testar Websockets

--- src/APISignalR/Startup.cs
+++ src/APISignalR/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using APISignalR.MessageServer;
 
 namespace APISignalR
 {
@@ -38,6 +39,10 @@ namespace APISignalR
             loggerFactory.AddDebug();
 
             app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Map("/ws", SocketHandler.Map);
         }
     }
 }
